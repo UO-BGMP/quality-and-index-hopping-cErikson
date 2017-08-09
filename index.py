8f03010e08c165a52b58685ba5520bdc65cc8647
cErikson <bio.erikson@gmail.com>
2017-08-09 12:13:12 -0700

    completed homework

--- index.py
+++ index.py
@@ -7,6 +7,8 @@ Filter out entries whos indexs are below a qual score, then seperates reads into
 """
 ###### Imports #####
 import argparse as arg
+import os.path
+import json
 
 ##### Args #####
 if __name__ == "__main__":
@@ -17,8 +19,10 @@ if __name__ == "__main__":
     parser.add_argument("index2_file", help="index 2 File", type=str)
     parser.add_argument("read2_file", help="read 2 File", type=str)
     parser.add_argument("known_file", help="Known indexes File", type=str)
+    parser.add_argument("index_col", help="Column in Known indexes File that contains barcodes ", type=int)
     parser.add_argument("out_dir", help="output dir", type=str)
     parser.add_argument("-c", "--cutoff", help="cutoff", type=int)
+    
     # Parse arguments
     ARGS = parser.parse_args()
 
@@ -42,11 +46,11 @@ known=[]
 codes=set()
 with open(ARGS.known_file,'r') as kf:
     for line in kf:
-        codes.add(line.split('\t')[1].strip())
+        codes.add(line.split('\t')[ARGS.index_col].strip())
 for c1 in codes:
     for c2 in codes:
         known.append((c1,c2))
-known.append(('unknown','index'))
+known.append('unknown_index')
 del codes
 
 # make output files
@@ -55,15 +59,26 @@ for code in known:
     files[code]=[open(ARGS.out_dir+code[0]+'_'+code[1]+x+'.fastq', 'w') for x in ('_R1','_I1', '_I2', '_R2')]
 del known
 
+# open counts file
+if os.path.isfile(ARGS.out_dir+'counts.json') is True:
+    with open(ARGS.out_dir+'counts.json', 'r') as fp:
+        counts = json.load(fp)
+else:
+    counts={}
+    
 # Filter, seperate, and write records
-counts={}
 for r1, i1, i2, r2 in zip(yield_fastq(ARGS.read1_file), yield_fastq(ARGS.index1_file), yield_fastq(ARGS.index2_file), yield_fastq(ARGS.read2_file)): # for each input fastq
         if all(x >= ARGS.cutoff for x in i1[2] and i2[2]): # if all the phred scores pass cutoff
-            counts.setdefault((i1[1],i2[1]),0) # set default index count
-            counts[(i1[1],i2[1])]+=1    # increment index count            
-            for fh, data in zip(files[(i1[1],i2[1]) if (i1[1], i2[1]) in files else ('unknown','index')], [r1, i1, i2, r2]):   # for each output file and its respective input entry, and determine if known          
+            counts.setdefault(i1[1]+'_'+i2[1],{ARGS.cutoff:0}) # set default tag
+            counts[i1[1]+'_'+i2[1]].setdefault(ARGS.cutoff,0) # Set default cutoff
+            counts[i1[1]+'_'+i2[1]][ARGS.cutoff]+=1    # increment index count            
+            for fh, data in zip(files[i1[1]+'_'+i2[1] if i1[1]+'_'+i2[1] in files else 'unknown_index'], [r1, i1, i2, r2]):   # for each output file and its respective input entry, and determine if known          
                 fh.write('{0}_{1}\n{2}\n+\n{3}\n'.format(data[0].split()[0], i1[1]+'_'+i2[1], data[1], ''.join((chr(x+33) for x in data[2])))) # write the fastq
-        
+
+# write JSON 
+with open(ARGS.out_dir+'counts.json', 'w') as fp:
+        json.dump(counts, fp, sort_keys=True, indent=2) 
+
 # close files                     
 for idx in files.values(): 
     for fh in idx: 
